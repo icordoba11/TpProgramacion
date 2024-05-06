f080b987e116b9e606b7d83ad11849184e33045a
nicol <nicol@DESKTOP-IOT929R>
2024-05-06 16:44:37 -0300

    Add project files.

---
+++ GordoIncognito/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.BackgroundColor = ConsoleColor.Black;
+        Console.ForegroundColor = ConsoleColor.DarkGreen;
+        Console.WriteLine("  ┌─────────────────────────────────────────────────────┐");
+        Console.WriteLine("  │                    AmericanAirlines                 |");
+        Console.WriteLine("  │-----------------------------------------------------│");
+        Console.WriteLine($"  │ Nombre: {"Jesus"}   Edad: {"12"}                           ");
+        Console.WriteLine($"  │ DNI:    {"24746224"}                                    ");
+        Console.WriteLine("  │                                                     ");
+        Console.WriteLine($"  │ Destino:                 {"destino"}                    ");
+        Console.WriteLine($"  │ Clase:                   {"Clase"}                      ");
+        Console.WriteLine($"  │ Equipaje de mano:        {"Si"}                         ");
+        Console.WriteLine($"  │ Equipaje bodega:         {"Si"}                         ");
+        Console.WriteLine($"  │ Recargo Equipaje:        {"$120"}                       ");
+        Console.WriteLine($"  │ Numero embarque:         {"AF569821"}                   ");
+        Console.WriteLine($"  │ Fecha:         05/05/2024         Hora: 10:00 AM    ");
+        Console.WriteLine($"  │ Asiento:       23A                  Puerta: 12      ");
+        Console.WriteLine($"  │                                                     ");
+        Console.WriteLine($"  │                                                     ");
+        Console.WriteLine("  |                                                     ");
+        Console.WriteLine("  │              ║||||||║||||||║                        ");
+        Console.WriteLine("  │              ║||||||║||||||║                        ");
+        Console.WriteLine("  │             5 901234 123456 0                       ");
+        Console.WriteLine("  └─────────────────────────────────────────────────────");
+        
+    }
+ 
+
+}
